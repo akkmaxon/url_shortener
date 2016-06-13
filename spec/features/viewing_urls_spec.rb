1669cd6c3f7c95ffdb37d76365f8d389898bb074
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-06-13 14:22:42 +0300

    Refactor tests on show index action

--- spec/features/viewing_urls_spec.rb
+++ spec/features/viewing_urls_spec.rb
@@ -1,34 +1,49 @@
 require 'rails_helper'
 
-RSpec.feature 'Signed in user can view all his urls' do
+RSpec.feature 'Show urls' do
   let(:user) { FactoryGirl.create(:user) }
-  let(:first_url) { FactoryGirl.build(:url, original: 'first') }
-  let(:second_url) { FactoryGirl.build(:url, original: 'second') }
-  let(:third_url) { FactoryGirl.build(:url, original: 'third') }
+  let(:first_url) { FactoryGirl.build(:url, original: 'first', short: '1') }
+  let(:second_url) { FactoryGirl.build(:url, original: 'second', short: '2') }
+  let(:third_url) { FactoryGirl.build(:url, original: 'third', short: '3') }
 
-  scenario 'successully' do
-    login_as user
-    [first_url, second_url, third_url].each do |url|
-      visit '/'
-      fill_in 'Original url', with: url.original
-      click_button 'Submit'
+  context 'for signed in user' do
+    before do
+      login_as user
+      [first_url, second_url, third_url].each do |url|
+	visit '/'
+	fill_in 'Original url', with: url.original
+	click_button 'Submit'
+      end
+      click_link 'View all urls'
     end
-    click_link 'View all urls'
-    
-    expect(page).to have_content first_url.original
-    expect(page).to have_content second_url.original
-    expect(page).to have_content third_url.original
+
+    scenario 'index action' do
+      [first_url, second_url, third_url].each do |url|
+	expect(page).to have_content url.short
+	expect(page).to have_content "(#{url.original})"
+      end
+    end
+
+    scenario 'show action' do
+    end
   end
 
-  scenario 'but not anonymous' do
-    [first_url, second_url, third_url].each do |url|
-      visit '/'
-      fill_in 'Original url', with: url.original
-      click_button 'Submit'
+  context 'for anonymous user' do
+    before do
+      [first_url, second_url, third_url].each do |url|
+	visit '/'
+	fill_in 'Original url', with: url.original
+	click_button 'Submit'
+      end
     end
-    
-    expect(page).to_not have_link 'View all urls'
-    visit '/urls'
-    expect(page).to have_content 'You need to sign in or sign up before continuing'
+
+    scenario 'index action' do
+      expect(page).to_not have_link 'View all urls'
+      visit '/urls'
+      expect(page).to have_content 'You need to sign in or sign up before continuing'
+    end
+
+    scenario 'show action' do
+    end
   end
 end
